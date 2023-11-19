f7057d020f99ca1fea94584b52064adfd3f26cb5
Kushagr Narayan <75263274+Kushagr06@users.noreply.github.com>
2023-11-19 19:51:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// src/App.js
+import React, { useState } from 'react';
+import Header from './components/Header';
+import BookingForm from './components/BookingForm';
+import BookingList from './components/BookingList';
+
+const App = () => {
+  const [bookings, setBookings] = useState([]);
+
+  const handleBooking = (booking) => {
+    // Update state with new booking
+    setBookings([...bookings, booking]);
+  };
+
+  return (
+    <div style={{"textAlign" : "center"}}>
+      <Header />
+      <BookingForm onBooking={handleBooking} />
+      <BookingList bookings={bookings} />
+    </div>
+  );
+};
+
+// Website by Kushagr Narayan
+export default App;
